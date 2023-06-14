eb3f692ab3d364db49e87cfb5d741bef2218136e
GMwanda <98454661+GMwanda@users.noreply.github.com>
2023-06-14 22:24:46 +0300

    Email Verification

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
+ 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -34,3 +36,24 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 // RECRUITERS ROUTES
 Route::get('/company_home', [RecruitersController::class, 'home'])->name('recruiters_homePage');
 Route::get('/company_registration', [RecruitersController::class, 'registration'])->name('company_registration');
+
+
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+ 
+    return redirect('/home');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+ 
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+
+Route::get('/profile', function () {
+    // Only verified users may access this route...
+})->middleware(['auth', 'verified']);
