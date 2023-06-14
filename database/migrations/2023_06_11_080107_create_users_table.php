f3f992854a3638e3678516401624c710f7baed0f
GMwanda <98454661+GMwanda@users.noreply.github.com>
2023-06-14 16:11:10 +0300

    Rectified migration error

--- database/migrations/2023_06_11_080107_create_users_table.php
+++ database/migrations/2023_06_11_080107_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->foreignId('role_id')->constrained()->onDelete('cascade');
+            $table->foreign('id')->references('id')->on('user_roles');
             $table->rememberToken();
             $table->timestamps();
         });
