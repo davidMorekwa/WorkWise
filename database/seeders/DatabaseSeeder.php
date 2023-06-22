6c260337e3bd7a6550c4a39c53907a4029d29cc3
GMwanda <98454661+GMwanda@users.noreply.github.com>
2023-06-22 08:22:47 +0300

    Seeder changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-
+use App\Models\JobPost;
+use App\Models\User;
 use App\Models\UserRole;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -17,7 +18,21 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+
+        // DB::table('user_roles')->insert([
+        //     'Roles'=>"Admin",
         // ]);
+        // DB::table('user_roles')->insert([
+        //     'Roles'=>"Recruiter",
+        // ]);
+        // DB::table('user_roles')->insert([
+        //     'Roles'=>"Job Seeker",
+
         // User::factory(5)->create();
         JobPost::factory(5)->create();
     }
