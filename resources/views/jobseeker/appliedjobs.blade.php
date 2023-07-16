80c826877664234d0a05e7967df7baea35844598
GMwanda <98454661+GMwanda@users.noreply.github.com>
2023-07-16 20:43:19 +0300

    View Application

--- resources/views/jobseeker/appliedjobs.blade.php
+++ resources/views/jobseeker/appliedjobs.blade.php
@@ -1,5 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>New Home Test</h1>
+    @if ($applications->isEmpty())
+        <h2 style="text-align:center; padding-top:4rem; font-size:1.8rem;">There are no Job postings made yet.
+        </h2>
+    @else
+        @foreach ($applications as $applied)
+            <fieldset>
+                <div class="card" id="cards">
+                    <div style="font-size: 1.6rem;font-weight: 600; color: #894c75;">
+                        <p>Job resume: {{ $applied->resume }}</p>
+                    </div>
+                    {{-- <div style="font-style: italic">
+                        <p>Job Type: {{ $applied->type }}</p>
+                    </div>
+                    <div>
+                        <p>OVERVIEW: <br />{!! $applied->overview !!}</p>
+                    </div> --}}
+                </div>
+            </fieldset>
+        @endforeach
+    @endif
 @endsection
